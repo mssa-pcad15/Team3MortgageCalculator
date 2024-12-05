dafab5e9c758738be075b4a9b23c8f436df62fa5
jojo <joel.mangum@git.com>
2024-12-05 15:06:15 -0800

    Test Customer Class

--- MortgageCalculator/Program.cs
+++ MortgageCalculator/Program.cs
@@ -7,31 +7,19 @@ namespace MortgageCalculator
         {
 
         }
-        // M = P [ i (1 + i)^n ] / [ (1 + i)^n – 1]
-        public static decimal CalculateMonthlyPayment(decimal loanAmount, decimal monthlyInterest, int LoanTimeInYears)
-        {
-            decimal monthlyRate = monthlyInterest / 100/12;
 
-            int totalPayments = LoanTimeInYears * 12;
-
-            decimal monthlyPayment = loanAmount *
-                (monthlyRate * (decimal)Math.Pow(1 + (double)monthlyRate, totalPayments)) 
-                /
-                ((decimal)Math.Pow(1 + (double)monthlyRate, totalPayments) - 1);
-            return Math.Round(monthlyPayment, 2);
-        }
         public class Customer
         {
             public string Name { get; set; }
-            public decimal HomePrice { get; set; }
-            public decimal DownPayment { get; set; }
-            public decimal LoanAmount => HomePrice - DownPayment;
-
-            public Customer(string name, decimal homePrice, decimal downPayment) // constructor
+            //public decimal HomePrice { get; set; }
+            //public decimal DownPayment { get; set; }
+            //public decimal LoanAmount => HomePrice - DownPayment;
+            public List<Mortgage> houses = new List<Mortgage>();
+            public Customer(string name/*, decimal homePrice, decimal downPayment*/) // constructor
             {
                 Name = name;
-                HomePrice = homePrice;
-                DownPayment = downPayment;
+                //    HomePrice = homePrice;
+                //    DownPayment = downPayment;
             }
         }
 
