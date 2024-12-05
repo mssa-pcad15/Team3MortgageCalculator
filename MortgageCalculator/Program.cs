f8e864dd355dbed636846092a174a5783051e9f9
emerson <sd543tv@gmail.com>
2024-12-05 09:57:01 -0800

    implemented initial mortgage calc method, as well as customer class + constructor

--- MortgageCalculator/Program.cs
+++ MortgageCalculator/Program.cs
@@ -2,6 +2,34 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
+    // M = P [ i (1 + i)^n ] / [ (1 + i)^n – 1]
+    public decimal CalculateMonthlyPayment(decimal loanAmount, decimal monthlyInterest, int LoanTimeInYears)
+    {
+        decimal monthlyRate = monthlyInterest / 100;
+
+        int totalPayments = LoanTimeInYears * 12;
+
+        decimal monthlyPayment = loanAmount *
+            (monthlyRate * (decimal)Math.Pow(1 + (double)monthlyRate, totalPayments)) /
+            ((decimal)Math.Pow(1 + (double)monthlyRate, totalPayments) - 1);
+        return Math.Round(monthlyPayment, 2);
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public decimal HomePrice { get; set; }
+        public decimal DownPayment { get; set; }
+        public decimal LoanAmount => HomePrice - DownPayment;
+        
+        public Customer(string name, decimal homePrice, decimal downPayment) // constructor
+        {
+            Name = name;
+            HomePrice = homePrice;
+            DownPayment = downPayment;
+        }
+    }
+
+
 }
