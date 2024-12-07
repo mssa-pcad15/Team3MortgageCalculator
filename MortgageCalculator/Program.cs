b4a8dfd1c53c35984bd1812afe04788f370d6f4d
emerson <sd543tv@gmail.com>
2024-12-07 15:04:10 -0800

    implemented test validation yml

--- MortgageCalculator/Program.cs
+++ MortgageCalculator/Program.cs
@@ -9,19 +9,23 @@ namespace MortgageCalculator
             decimal monthlyPayment = MortgageCalculator.CalculateMonthlyPayment(mortgage);
             Console.WriteLine($"monthly payment: {monthlyPayment}");
         }
-        // M = P [ i (1 + i)^n ] / [ (1 + i)^n – 1]
-        //public static decimal CalculateMonthlyPayment(decimal loanAmount, decimal monthlyInterest, int LoanTimeInYears)
-        //{
-        //    decimal monthlyRate = monthlyInterest / 100 / 12;
-        //
-        //    int totalPayments = LoanTimeInYears * 12;
-        //
-        //    decimal monthlyPayment = loanAmount *
-        //        (monthlyRate * (decimal)Math.Pow(1 + (double)monthlyRate, totalPayments))
-        //        /
-        //        ((decimal)Math.Pow(1 + (double)monthlyRate, totalPayments) - 1);
-        //    return Math.Round(monthlyPayment, 2);
-        //}
+        
+        /*
+        M = P [ i (1 + i)^n ] / [ (1 + i)^n – 1]
+        public static decimal CalculateMonthlyPayment(decimal loanAmount, decimal monthlyInterest, int LoanTimeInYears)
+        {
+            decimal monthlyRate = monthlyInterest / 100 / 12;
+        
+            int totalPayments = LoanTimeInYears * 12;
+        
+            decimal monthlyPayment = loanAmount *
+                (monthlyRate * (decimal)Math.Pow(1 + (double)monthlyRate, totalPayments))
+                /
+                ((decimal)Math.Pow(1 + (double)monthlyRate, totalPayments) - 1);
+            return Math.Round(monthlyPayment, 2);
+        }
+
+        */
         public class Customer
         {
             public string Name { get; set; }
