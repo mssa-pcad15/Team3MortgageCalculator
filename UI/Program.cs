d7d9c14980fc8bfb5514e99c87d324984b01c8c7
emerson <sd543tv@gmail.com>
2024-12-09 15:25:47 -0800

    implemented basic navigation menu to access account menu or mortgages menu.

--- UI/Program.cs
+++ UI/Program.cs
@@ -7,21 +7,94 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        MortgageCalculator.Program.Bank tdBank = new MortgageCalculator.Program.Bank();
-        MortgageCalculator.Program.Customer reese= new MortgageCalculator.Program.Customer("Reese");
-        MortgageCalculator.Program.Mortgage house = new MortgageCalculator.Program.Mortgage(200000, 6.734m, 30);
-        reese.houses.Add(house);
+        //MortgageCalculator.Program.Bank tdBank = new MortgageCalculator.Program.Bank();
+        //MortgageCalculator.Program.Customer reese= new MortgageCalculator.Program.Customer("Reese");
+        //MortgageCalculator.Program.Mortgage house = new MortgageCalculator.Program.Mortgage(200000, 6.734m, 30);
+        //reese.houses.Add(house);
+        //
+        //AnsiConsole.WriteLine($"Hello {reese.Name}! Welcome to {tdBank}");// add bank name to package
+        //
+        //
+        //AnsiConsole.Markup($"[green]Account[/]:{house.AccountNumber}\n[green]Loan Amount[/]:{house.LoanAmount}\n" +
+        //$"[green]Interest Rate[/]:{house.AnnualInterestRate:p}\n[green]Loan Length[/]:{house.LoanTimeInYears}");
 
-        AnsiConsole.WriteLine($"Hello {reese.Name}! Welcome to {tdBank}");// add bank name to package
+        var choice = String.Empty;
+        //while (choice != "Quit") ;
+        //{
+            choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Select one")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to select)[/]")
+                .AddChoices(new[] { "Mortgages", "Account", "Quit" }));
+            switch (choice)
+            {
+                case "Mortgages": ShowMortgagesMenu();
+                        break;
+                case "Account": ShowAccountMenu();
+                        break;
+                case "Quit": 
+                    return;
+            }
+        //}
+
+        static void ShowMortgagesMenu()
+        {
+            choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("What would you like to do?")
+            .PageSize(10)
+            .AddChoices(new[] {
+            "New Loan", "New Mortgage", "Remove Mortgage"
+            }));
+
+            switch (choice)
+            {
+                case "New Loan":
+                    MortgagesNewLoan();
+                    break;
+                case "New Mortgage":
+                    MortgagesNewMortgage();
+                    break;
+                case "Remove Mortgage":
+                    MortgagesRemoveMortgage();
+                    break;
+                case "Quit":
+                    return;
+            }
+        }
+
+        static void ShowAccountMenu()
+        {
+            choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("What would you like to do?")
+            .PageSize(10)
+            .AddChoices(new[] {
+            "My Mortgages", "My Account Information", "Account Settings"
+            }));
+
+            switch (choice)
+            {
+                case "My Mortgages":
+                    CustomerMortgages._mortgages;
+                    break;
+                case "My Account Information":
+                    CustomerAccount.Information;
+                    break;
+                case "Account Settings":
+                    CustomerAccountSettings();
+                    break;
+                case "Quit":
+                    return;
+            }
+        }
 
 
-        AnsiConsole.Markup($"[green]Account[/]:{house.AccountNumber}\n[green]Loan Amount[/]:{house.LoanAmount}\n" +
-        $"[green]Interest Rate[/]:{house.AnnualInterestRate:p}\n[green]Loan Length[/]:{house.LoanTimeInYears}");
 
 
 
 
-        
 
 
 
