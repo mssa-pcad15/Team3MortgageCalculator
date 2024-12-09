05276f947b505fb9a7e962511e6bb0fba042c47c
jojo <joel.mangum@git.com>
2024-12-09 08:43:35 -0800

    tests

--- MortgageCalculatorTest/UnitTest1.cs
+++ MortgageCalculatorTest/UnitTest1.cs
@@ -27,7 +27,7 @@ namespace MortgageCalculatorTest
         public void TestMonthlyMortgagePayment()
         {
             decimal M = 1295.07m;//monthly mortgage
-            decimal P =200000 ;//principal amount
+            decimal P = 200000;//principal amount
             decimal i = 6.734m;//interest rate
             int n = 30;//loan length in years
 
@@ -35,22 +35,22 @@ namespace MortgageCalculatorTest
 
             decimal testValue = Program.MortgageCalculator.CalculateMonthlyPayment(mortgage);
 
-            Assert.AreEqual(M, testValue);    
+            Assert.AreEqual(M, testValue);
         }
         [TestMethod]
         public void TestMortgageClass()
         {
             decimal M = 1295.07m;//monthly mortgage
-           
-            
-            
+
+
+
             decimal P = 200000;//principal amount ---
             decimal i = 6.734m;//interest rate      |-------assert these are the same in the class using the getter
             int n = 30;//loan length in years -------
 
             Program.Mortgage mortgage = new Program.Mortgage(P, i, n);
 
-           
+
 
             Assert.AreEqual(P, mortgage.LoanAmount);//like this(now do it for annualInterestRate, and loanTimeInYears)
 
@@ -60,32 +60,34 @@ namespace MortgageCalculatorTest
         [TestMethod]
         public void TestCustomerClassMortgageList()
         {
-            Customer sally=new Customer("Sally");
-          
+            Customer sally = new Customer("Sally");
+            //HOUSE 1 250k
             decimal M1 = 1295.07m;//monthly mortgage
             decimal P1 = 200000;//principal amount
             decimal i1 = 6.734m;//interest rate
             int n1 = 30;//loan length in years
 
-
-            decimal M2 = 2339.45m;//monthly mortgage
-            decimal P2 = 540000;//principal amount
+            //HOUSE 2 600k
+            decimal M2 = 3273.55m;//monthly mortgage
+            decimal P2 = 546000;//principal amount
             decimal i2 = 6.00m;//interest rate
             int n2 = 30;//loan length in years
 
-            decimal M3 = 9153.34m;//monthly mortgage
-            decimal P3 = 1150000;//principal amount
-            decimal i3 = 6.7m;//interest rate
+            //HOUSE 3 1250000
+            decimal M3 = 6520.11m;//monthly mortgage
+            decimal P3 = 1087500;//principal amount
+            decimal i3 = 6.00m;//interest rate
             int n3 = 30;//loan length in years
 
-            decimal M4 = 4162.50m;//monthly mortgage
-            decimal P4 = 9990000;//principal amount
-            decimal i4 = 10m;//interest rate
+            //HOUSE 4 1000000
+            decimal M4 = 5188.03m;//monthly mortgage
+            decimal P4 = 800000;//principal amount
+            decimal i4 = 6.00m;//interest rate
             int n4 = 30;//loan length in years
 
-            sally.houses =new List<Mortgage>
-            { 
-                    new Mortgage(P1,i1,n1), 
+            sally.houses = new List<Mortgage>
+            {
+                    new Mortgage(P1,i1,n1),
                     new Mortgage(P2, i2, n2),
                     new Mortgage(P3, i3, n3),
                     new Mortgage(P4, i4, n4)
@@ -104,14 +106,42 @@ namespace MortgageCalculatorTest
             Assert.AreEqual(n1, sally.houses[0].LoanTimeInYears);
             Assert.AreEqual(n2, sally.houses[1].LoanTimeInYears);
             Assert.AreEqual(n3, sally.houses[2].LoanTimeInYears);
-            Assert.AreEqual(n4, sally.houses[3].LoanTimeInYears);
+            Assert.AreEqual(n4, sally.houses[3].LoanTimeInYears);            
+        }
+
+        [TestMethod]
+        public void TestCustomerMortgageListUsingCalculateMortgage()
+        {
+            Customer sally = new Customer("Sally");
+
+            //HOUSE 1 250k
+            decimal M1 = 1295.07m;//monthly mortgage
+            decimal P1 = 200000;//principal amount
+            decimal i1 = 6.734m;//interest rate
+            int n1 = 30;//loan length in years
+
+            //HOUSE 2 600k
+            decimal M2 = 3273.55m;//monthly mortgage
+            decimal P2 = 546000;//principal amount
+            decimal i2 = 6.00m;//interest rate
+            int n2 = 30;//loan length in years
+
+            //HOUSE 3 1250000
+            decimal M3 = 6520.11m;//monthly mortgage
+            decimal P3 = 1087500;//principal amount
+            decimal i3 = 6.00m;//interest rate
+            int n3 = 30;//loan length in years
+
+            //HOUSE 4 1000000
+            decimal M4 = 5188.03m;//monthly mortgage
+            decimal P4 = 800000;//principal amount
+            decimal i4 = 6.00m;//interest rate
+            int n4 = 30;//loan length in years
 
             Assert.AreEqual(M1, Program.MortgageCalculator.CalculateMonthlyPayment(sally.houses[0]));
             //Assert.AreEqual(M2, Program.MortgageCalculator.CalculateMonthlyPayment(sally.houses[1]));
             //Assert.AreEqual(M3, Program.MortgageCalculator.CalculateMonthlyPayment(sally.houses[2]));
             //Assert.AreEqual(M4, Program.MortgageCalculator.CalculateMonthlyPayment(sally.houses[3]));
-
         }
-
     }
 }
