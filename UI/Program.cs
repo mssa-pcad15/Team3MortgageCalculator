57dd8140f3007f42a31e5b2b4669e84f2219a112
jojo <joel.mangum@git.com>
2024-12-10 11:57:28 -0800

    Storing customer in function

--- UI/Program.cs
+++ UI/Program.cs
@@ -2,12 +2,12 @@
 internal class Program
 {
 
-    private static MortgageCalculator.Program.Customer currentCustomer;
+    
     private static void Main(string[] args)
     {
-        currentCustomer = new MortgageCalculator.Program.Customer("Reese");
-        currentCustomer.houses.Add(new MortgageCalculator.Program.Mortgage(180000, 6.734m, 30));
-        currentCustomer.houses.Add(new MortgageCalculator.Program.Mortgage(150000, 5.5m, 15));
+        //currentCustomer = new MortgageCalculator.Program.Customer("Reese");
+        //currentCustomer.houses.Add(new MortgageCalculator.Program.Mortgage(180000, 6.734m, 30));
+        //currentCustomer.houses.Add(new MortgageCalculator.Program.Mortgage(150000, 5.5m, 15));
         //MortgageCalculator.Program.Bank tdBank = new MortgageCalculator.Program.Bank();
         //MortgageCalculator.Program.Customer reese= new MortgageCalculator.Program.Customer("Reese");
         //MortgageCalculator.Program.Mortgage house = new MortgageCalculator.Program.Mortgage(200000, 6.734m, 30);
@@ -19,68 +19,42 @@ internal class Program
         //AnsiConsole.Markup($"[green]Account[/]:{house.AccountNumber}\n[green]Loan Amount[/]:{house.LoanAmount}\n" +
         //$"[green]Interest Rate[/]:{house.AnnualInterestRate:p}\n[green]Loan Length[/]:{house.LoanTimeInYears}");
         //MortgageCalculator.Program.Bank List<MortgageCalculator.Program.Bank>
-
         List<MortgageCalculator.Program.Bank> Corpos = new List<MortgageCalculator.Program.Bank>();
-        //---------------------
-        var bankName = AnsiConsole.Prompt(
-                new TextPrompt<string>("What bank will you be working with today?"));
-        MortgageCalculator.Program.Bank newBank = new MortgageCalculator.Program.Bank(bankName);
 
-        Corpos.Add(newBank);
-        //---------------------
-        var customerName = AnsiConsole.Prompt(
-                new TextPrompt<string>("What is your Name?"));
-        MortgageCalculator.Program.Customer newCustomer = new MortgageCalculator.Program.Customer(customerName);
-
-        newBank.customers.Add(newCustomer);
-
-        //---------------------
-        AnsiConsole.WriteLine($"Hello {newCustomer.Name}! Welcome to {newBank.BankName}!\nHow Can We Help You?");// add bank name to package
-        //==================
+        Initialize(Corpos);
 
 
-
-        ShowAccountMenu();
-
-
-
-
-
-
-        static void Initialize()
-        {
-            var bankName = AnsiConsole.Prompt(
-                 new TextPrompt<string>("What bank will you be working with today?"));
-            MortgageCalculator.Program.Bank tdBank = new MortgageCalculator.Program.Bank(bankName);
-
-
-        }
-
-
-        var choice = String.Empty;
-        do
-        {
-            choice = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                .Title("Select one")
-                .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to select)[/]")
-                .AddChoices(new[] { "Mortgages", "Account", "Quit" }));
-            switch (choice)
-            {
-                case "Mortgages":
-                    ShowMortgagesMenu();
-                    break;
-                case "Account":
-                    ShowAccountMenu();
-                    break;
-                case "Quit":
-                    return;
-            }
-        } while (choice != "Quit");
 
     }
-    static void ShowMortgagesMenu()
+
+
+
+    static void Initialize(List<MortgageCalculator.Program.Bank> corporation)
+    {
+        
+            //---------------------
+            var bankName = AnsiConsole.Prompt(
+                    new TextPrompt<string>("What bank will you be working with today?"));
+            MortgageCalculator.Program.Bank newBank = new MortgageCalculator.Program.Bank(bankName);
+
+            corporation.Add(newBank);
+            //---------------------
+            var customerName = AnsiConsole.Prompt(
+                    new TextPrompt<string>("What is your Name?"));
+            MortgageCalculator.Program.Customer newCustomer = new MortgageCalculator.Program.Customer(customerName);
+
+            newBank.customers.Add(newCustomer);
+
+            //---------------------
+            AnsiConsole.WriteLine($"Hello {newCustomer.Name}! Welcome to {newBank.BankName}!\nHow Can We Help You?");// add bank name to package
+        while (true)
+        {                                                                                               //==================
+            ShowMenu(newCustomer);
+        }
+    }
+
+
+    static void ShowMortgagesMenu(MortgageCalculator.Program.Customer customer)
     {
         var choice = String.Empty;
         choice = AnsiConsole.Prompt(
@@ -94,10 +68,10 @@ internal class Program
         switch (choice)
         {
             case "Show Monthly Payments":
-                ShowMonthlyPayment();
+                ShowMonthlyPayment(customer);
                 break;
             case "Remove Mortgage":
-                RemoveMortgage();
+                RemoveMortgage(customer);
                 // when remove is selected, go to new menu
                 // in new menu, show mortgages, and select one to remove
                 break;
@@ -106,24 +80,25 @@ internal class Program
         }
     }
 
-    static void ShowMonthlyPayment()
+
+    static void ShowMonthlyPayment(MortgageCalculator.Program.Customer customer)
     {
-        if (currentCustomer.houses.Count == 0)
+        if (customer.houses.Count == 0)
         {
             AnsiConsole.WriteLine("You have no mortgages");
             return;
         }
-        foreach (var mortgage in currentCustomer.houses)
+        foreach (var mortgage in customer.houses)
         {
             decimal monthlyPayment = MortgageCalculator.Program.MortgageCalculator.CalculateMonthlyPayment(mortgage);
             AnsiConsole.WriteLine($"The monthly payment for {mortgage.AccountNumber} is {monthlyPayment}");
         }
-
     }
 
-    static void RemoveMortgage()
+
+    static void RemoveMortgage(MortgageCalculator.Program.Customer customer)
     {
-        if (currentCustomer.houses.Count == 0)
+        if (customer.houses.Count == 0)
         {
             AnsiConsole.WriteLine("You have no mortgages");
             return;
@@ -133,7 +108,7 @@ internal class Program
             new SelectionPrompt<string>()
             .Title("Select mortgage to remove")
             .PageSize(10)
-            .AddChoices(currentCustomer.houses.Select(h => h.AccountNumber).Concat(new string[] { "Cancel" }))
+            .AddChoices(customer.houses.Select(h => h.AccountNumber).Concat(new string[] { "Cancel" }))
             );
 
         switch (choice)
@@ -141,17 +116,18 @@ internal class Program
             case "Cancel":
                 return;
             default:
-                var mortgage = currentCustomer.houses.FirstOrDefault(h => h.AccountNumber == choice);
+                var mortgage = customer.houses.FirstOrDefault(h => h.AccountNumber == choice);
                 if (mortgage != null)
                 {
-                    currentCustomer.houses.Remove(mortgage);
+                    customer.houses.Remove(mortgage);
                     AnsiConsole.WriteLine($"Mortgage {mortgage.AccountNumber} removed");
                 }
                 break;
         }
     }
 
-    static void ShowAccountMenu()
+
+    static void ShowMenu(MortgageCalculator.Program.Customer customer)
     {
         var choice = String.Empty;
         choice = AnsiConsole.Prompt(
@@ -159,39 +135,100 @@ internal class Program
         .Title("What would you like to do?")
         .PageSize(10)
         .AddChoices(new[] {
-        "Show My Mortgages", "My Account Information", "Account Settings"
+        "Show My Mortgages", "My Account", "Account Options","Quit"
         }));
 
         switch (choice)
         {
             case "Show My Mortgages":
-
-                // when my mortgages is selected, go to new menu
-                // in new menu, show all mortgages
-                // --------------maybe-----------------
-                // choose to either remove or exit
-                // if remove, go to new context menu to select which to delete
-                ShowMyMortgages();
+                ShowMyMortgages(customer);
                 break;
-            case "My Account Information":
-                //CustomerAccount.Information;
+            case "My Account":
+                ShowAccountMenu(customer);
                 break;
-            case "Account Settings":
+            case "Account Options":
                 //CustomerAccountSettings();
                 break;
             case "Quit":
+                System.Environment.Exit(0); 
                 return;
         }
     }
-    public static void ShowMyMortgages()
+
+
+    static void ShowAccountOptions(MortgageCalculator.Program.Customer customer)
     {
-        if (currentCustomer.houses.Count == 0)
+
+    }
+
+        static void ShowAccountMenu(MortgageCalculator.Program.Customer customer)
+    {
+
+        var choice = String.Empty;
+        do
+        {
+            choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Select one")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to select)[/]")
+                .AddChoices(new[] { "Add Mortgage" ,"Mortgage Info", "Back" }));
+            switch (choice)
+            {
+                case "Add Mortgage":
+                    //add mortgage func
+                    break ;
+                case "Mortgages":
+                    ShowMortgagesMenu(customer);
+                    break;
+                case "Back":
+                    ShowMenu(customer);
+                    break;
+                
+            }
+        } while (choice != "Quit");
+
+
+        //var choice = String.Empty;
+        //choice = AnsiConsole.Prompt(
+        //new SelectionPrompt<string>()
+        //.Title("What would you like to do?")
+        //.PageSize(10)
+        //.AddChoices(new[] {
+        //"Show My Mortgages", "My Account Information", "Account Settings"
+        //}));
+
+        //switch (choice)
+        //{
+        //    case "Show My Mortgages":
+
+        //        // when my mortgages is selected, go to new menu
+        //        // in new menu, show all mortgages
+        //        // --------------maybe-----------------
+        //        // choose to either remove or exit
+        //        // if remove, go to new context menu to select which to delete
+        //        ShowMyMortgages();
+        //        break;
+        //    case "My Account Information":
+        //        //CustomerAccount.Information;
+        //        break;
+        //    case "Account Settings":
+        //        //CustomerAccountSettings();
+        //        break;
+        //    case "Quit":
+        //        return;
+        //}
+    }
+
+    public static void ShowMyMortgages(MortgageCalculator.Program.Customer customer)
+    {
+        if (customer.houses.Count == 0)
         {
             AnsiConsole.WriteLine("No mortgages found.");
             return;
         }
 
-        foreach (var mortgage in currentCustomer.houses)
+        foreach (var mortgage in customer.houses)
         {
             AnsiConsole.Markup($"\n[green]Loan Amount[/]: {mortgage.LoanAmount}\n" +
                                $"[green]Interest Rate[/]: {mortgage.AnnualInterestRate}%\n" +
